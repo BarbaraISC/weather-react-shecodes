2fc42a7c7f9de657f45a8477ab1beafae747f900
BarbaraISC <barbaraisc93@gmail.com>
2025-04-29 11:07:55 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -9,16 +9,16 @@ function App() {
         <img className="logo" alt="SheCodes logo" src={logo} />
         <h1>Weather App</h1>
         <WeatherForm />
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/BarbaraISC">Bárbara Silva</a> and is{" "}
+          <a href="https://github.com/BarbaraISC/weather-react-shecodes">
+            open-sourced on Github
+          </a>{" "}
+          and hosted on{" "}
+          <a href="https://weather-app-barbara.netlify.app/">Netlify</a>
+        </footer>
       </div>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/BarbaraISC">Bárbara Silva</a> and is{" "}
-        <a href="https://github.com/BarbaraISC/weather-react-shecodes">
-          open-sourced on Github
-        </a>{" "}
-        and hosted on{" "}
-        <a href="https://weather-app-barbara.netlify.app/">Netlify</a>
-      </footer>
     </div>
   );
 }
