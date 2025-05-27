f2b078c1944a23de149bf92116d3e0095eecac2b
BarbaraISC <barbaraisc93@gmail.com>
2025-05-27 14:24:51 +0100

    Added a new component with the first day of the forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,20 +1,35 @@
-import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
+import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
-export default function WeatherForecast() {
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="WeatherForecast-day">Thu</div>
-          <WeatherIcon code="clear-sky-day" size={36} />
-          <div className="WeatherForecast-temperatures">
-            <span className="WeatherForecast-temperature-max">19º</span>
-            <span className="WeatherForecast-temperature-min">10º</span>
+export default function WeatherForecast(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState("");
+
+  useEffect(() => {
+    setLoaded(false);
+
+    let apiKey = "43b563a3f016fb0f8t1650905fb6o494";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${props.city}&key=${apiKey}`;
+
+    axios.get(apiUrl).then((response) => {
+      setForecast(response.data.daily);
+      setLoaded(true);
+    });
+  }, [props.city]);
+
+  if (!loaded) {
+    return null;
+  } else {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
